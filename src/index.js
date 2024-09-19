da6a50ed01a0e5da575dee64c4d7e055723a4923
HadisYJ <hadis.yadollahi@gmail.com>
2024-09-19 15:50:17 +0200

    Updated index.html file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-function showAlert(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY H:m:s A");
-    alert(`It is ${currentTime} in ${event.target.value}`);
+function updateCityTime(cityId, timeZone) {
+  let cityElement = document.querySelector(`#${cityId}`);
+  if (cityElement) {
+    let cityDateElement = cityElement.querySelector(".date");
+    let cityTimeElement = cityElement.querySelector(".time");
+    let cityTime = moment().tz(timeZone);
+
+    cityDateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+    cityTimeElement.innerHTML = cityTime.format("h:mm:ss [<small>]A[</small>]");
   }
 }
+function updateTime() {
+  updateCityTime("los-angeles", "America/Los_Angeles");
+  updateCityTime("berlin", "Europe/Berlin");
+  updateCityTime("tehran", "Asia/Tehran");
+  updateCityTime("johannesburg", "Africa/Johannesburg");
+  updateCityTime("sydney", "Australia/Sydney");
+}
 
-let city = document.querySelector("#citiesSelect");
-city.addEventListener("change", showAlert);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "currentLocation") {
+    cityTimeZone = moment.tz.guess();
+  }
+
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cityRow");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+     <div class="cityName">${cityName}</div>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#citySelect");
+citiesSelectElement.addEventListener("change", updateCity);
